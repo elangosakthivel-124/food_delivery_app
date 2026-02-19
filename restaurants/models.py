b037f11ce7bfb683b25c6528561f97375a7a1b86
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-19 23:07:11 +0530

    Update models.py

--- restaurants/models.py
+++ restaurants/models.py
@@ -23,3 +23,44 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+        class Category(models.Model):
+
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.restaurant.name} - {self.name}"
+class FoodItem(models.Model):
+
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        related_name='food_items'
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    image = models.ImageField(upload_to='food_items/')
+
+    is_available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
