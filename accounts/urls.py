d2163cab0a83b935765c0af06367246316ac21a6
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-21 23:03:07 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,7 @@ from .views import RegisterAPIView, LoginAPIView
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
+    path('admin/', admin.site.urls),
+    path('api/', include('restaurants.urls')),
+    path('api/auth/', include('accounts.urls')),
 ]
