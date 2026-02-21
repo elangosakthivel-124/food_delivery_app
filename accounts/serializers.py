6d44784c36c417c765d903422cfb2730a6a831c5
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-21 23:00:33 +0530

    Create serializers.py

---
+++ accounts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'user_type']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            user_type=validated_data['user_type']
+        )
+        return user
