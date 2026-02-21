f41a0a54b461dead42d18272a2e04963e827d6ab
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-21 22:59:47 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,13 @@ INSTALLED_APPS = [
     'orders',
 ]
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
+INSTALLED_APPS = [
+    'rest_framework.authtoken',
+]
