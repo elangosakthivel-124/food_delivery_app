7791cdfbf89e0c1b5d526eb2fbe411fa763801ca
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-22 22:34:13 +0530

    Create serializers.py

---
+++ orders/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Cart, CartItem, Order, OrderItem
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items']
