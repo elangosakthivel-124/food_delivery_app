4fcb33d57966e7063887dc508355cad0b745500f
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-22 22:31:40 +0530

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -28,3 +28,25 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.food_item.name} ({self.quantity})"
+class Order(models.Model):
+
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('delivered', 'Delivered'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
