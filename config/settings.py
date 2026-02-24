88f35de5cf5b61f9299a80fe80ee55e8b3a69630
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-24 23:37:04 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,3 +23,21 @@ REST_FRAMEWORK = {
 INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
+INSTALLED_APPS += ['django_filters']
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 10
+}
