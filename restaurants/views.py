8cdfd3e9315047d944fb944d9eacad9840fb414a
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-25 09:49:46 +0530

    Update views.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -58,3 +58,9 @@ class RestaurantListAPIView(generics.ListCreateAPIView):
 from rest_framework.permissions import IsAuthenticated
 from .models import Review
 from .serializers import ReviewSerializer
+class CreateReviewAPIView(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
