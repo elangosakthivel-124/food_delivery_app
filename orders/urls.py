8b94ffaca6f13366e7fa3af666fdb30205521823
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-25 09:52:41 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -6,3 +6,24 @@ urlpatterns = [
     path('order/place/', PlaceOrderAPIView.as_view()),
     path('orders/', UserOrdersAPIView.as_view()),
 ]
+from .views import (
+    AddToCartAPIView,
+    PlaceOrderAPIView,
+    UserOrdersAPIView,
+    RestaurantOrdersAPIView,
+    UpdateOrderStatusAPIView,
+    RestaurantDashboardAPIView,
+    PopularFoodsAPIView
+)
+
+urlpatterns = [
+    path('cart/add/', AddToCartAPIView.as_view()),
+    path('order/place/', PlaceOrderAPIView.as_view()),
+    path('orders/', UserOrdersAPIView.as_view()),
+
+    path('restaurant/orders/', RestaurantOrdersAPIView.as_view()),
+    path('restaurant/dashboard/', RestaurantDashboardAPIView.as_view()),
+    path('restaurant/order/<int:order_id>/update/', UpdateOrderStatusAPIView.as_view()),
+
+    path('popular-foods/', PopularFoodsAPIView.as_view()),
+]
