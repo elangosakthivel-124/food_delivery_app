fa6c740724a9f8b7f13acd8c43ad53406b72dfcd
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-26 23:42:36 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -12,3 +12,25 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+class Address(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='addresses'
+    )
+
+    full_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+
+    address_line = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=10)
+
+    is_default = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.full_name} - {self.city}"
