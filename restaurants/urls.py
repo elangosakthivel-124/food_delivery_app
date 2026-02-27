1adaa60589dd16b81c3520b3925cf140de4889c3
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-27 23:58:34 +0530

    Update urls.py

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,52 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('restaurants.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from django.urls import path
 from .views import (
-    RestaurantListAPIView,
-    RestaurantDetailAPIView,
-    FoodItemListAPIView,
-    FoodItemDetailAPIView,
-    CategoryListAPIView
+    RestaurantListCreateView,
+    RestaurantDetailView,
+    MenuItemListCreateView,
+    MenuItemDetailView,
 )
 
 urlpatterns = [
-    path('restaurants/', RestaurantListAPIView.as_view()),
-    path('restaurants/<int:pk>/', RestaurantDetailAPIView.as_view()),
-
-    path('foods/', FoodItemListAPIView.as_view()),
-    path('foods/<int:pk>/', FoodItemDetailAPIView.as_view()),
-
-    path('categories/', CategoryListAPIView.as_view()),
-]
-from .views import (
-    RestaurantListAPIView,
-    RestaurantDetailAPIView,
-    FoodItemListAPIView,
-    FoodItemDetailAPIView,
-    CategoryListAPIView,
-    CreateReviewAPIView,
-    TopRestaurantsAPIView
-)
-
-urlpatterns = [
-    path('restaurants/', RestaurantListAPIView.as_view()),
-    path('restaurants/<int:pk>/', RestaurantDetailAPIView.as_view()),
-
-    path('foods/', FoodItemListAPIView.as_view()),
-    path('foods/<int:pk>/', FoodItemDetailAPIView.as_view()),
-
-    path('categories/', CategoryListAPIView.as_view()),
-
-    path('review/', CreateReviewAPIView.as_view()),
-    path('top-restaurants/', TopRestaurantsAPIView.as_view()),
+    path("", RestaurantListCreateView.as_view(), name="restaurants"),
+    path("<int:pk>/", RestaurantDetailView.as_view(), name="restaurant-detail"),
+    path("menu/", MenuItemListCreateView.as_view(), name="menu-items"),
+    path("menu/<int:pk>/", MenuItemDetailView.as_view(), name="menu-item-detail"),
 ]
