f2c6dba1d189b9d1bbd132e1e1e676ac17f91bbf
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-27 23:57:23 +0530

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,68 +1,23 @@
 from rest_framework import serializers
-from .models import User
-
+from django.contrib.auth.models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
-
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'user_type']
+        fields = ["id", "username", "email", "password"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            user_type=validated_data['user_type']
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
         )
         return user
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
 
 
-class LoginSerializer(serializers.Serializer):
-
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-
-        user = authenticate(
-            username=data['username'],
-            password=data['password']
-        )
-
-        if not user:
-            raise serializers.ValidationError("Invalid credentials")
-
-        token, created = Token.objects.get_or_create(user=user)
-
-        return {
-            'user': user,
-            'token': token.key
-        }
-from rest_framework import serializers
-from .models import User, Address
-
-
-class AddressSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Address
-        fields = '__all__'
-        read_only_fields = ['user']
-
-class UserProfileSerializer(serializers.ModelSerializer):
-
-    addresses = AddressSerializer(many=True, read_only=True)
-
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'user_type', 'addresses']class UserProfileSerializer(serializers.ModelSerializer):
-
-    addresses = AddressSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'user_type', 'addresses']
+        fields = ["id", "username", "email"]
