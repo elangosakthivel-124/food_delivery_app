bb4f2ab83313617cd1c2e05eaa83967f81dbcef6
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-27 23:58:08 +0530

    Update serializers.py

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -1,45 +1,14 @@
 from rest_framework import serializers
-from .models import Restaurant, Category, FoodItem
+from .models import Restaurant, MenuItem
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Restaurant
-        fields = '__all__'
+        fields = "__all__"
 
 
-class CategorySerializer(serializers.ModelSerializer):
-
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Category
-        fields = '__all__'
-
-
-class FoodItemSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = FoodItem
-        fields = '__all__'
-class FoodItemSerializer(serializers.ModelSerializer):
-
-    restaurant_name = serializers.CharField(
-        source='restaurant.name',
-        read_only=True
-    )
-
-    category_name = serializers.CharField(
-        source='category.name',
-        read_only=True
-    )
-
-    class Meta:
-        model = FoodItem
-        fields = '__all__'
-from .models import Restaurant, Category, FoodItem, Review
-
-class ReviewSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Review
-        fields = '__all__'
+        model = MenuItem
+        fields = "__all__"
