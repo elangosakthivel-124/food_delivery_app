86b207b22c9fa8c0b47985a429aaa5487cab93d2
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-27 23:59:26 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,29 +1,7 @@
 from django.urls import path
-from .views import AddToCartAPIView, PlaceOrderAPIView, UserOrdersAPIView
+from .views import OrderListCreateView, OrderDetailView
 
 urlpatterns = [
-    path('cart/add/', AddToCartAPIView.as_view()),
-    path('order/place/', PlaceOrderAPIView.as_view()),
-    path('orders/', UserOrdersAPIView.as_view()),
-]
-from .views import (
-    AddToCartAPIView,
-    PlaceOrderAPIView,
-    UserOrdersAPIView,
-    RestaurantOrdersAPIView,
-    UpdateOrderStatusAPIView,
-    RestaurantDashboardAPIView,
-    PopularFoodsAPIView
-)
-
-urlpatterns = [
-    path('cart/add/', AddToCartAPIView.as_view()),
-    path('order/place/', PlaceOrderAPIView.as_view()),
-    path('orders/', UserOrdersAPIView.as_view()),
-
-    path('restaurant/orders/', RestaurantOrdersAPIView.as_view()),
-    path('restaurant/dashboard/', RestaurantDashboardAPIView.as_view()),
-    path('restaurant/order/<int:order_id>/update/', UpdateOrderStatusAPIView.as_view()),
-
-    path('popular-foods/', PopularFoodsAPIView.as_view()),
+    path("", OrderListCreateView.as_view(), name="orders"),
+    path("<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
 ]
