d962f9ab70859e44ca2ed3fa462b7f85c0a000d3
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-27 23:58:22 +0530

    Update views.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,66 +1,25 @@
-from rest_framework import generics
-from .models import Restaurant, FoodItem
-from .serializers import RestaurantSerializer, FoodItemSerializer
+from rest_framework import generics, permissions
+from .models import Restaurant, MenuItem
+from .serializers import RestaurantSerializer, MenuItemSerializer
 
 
-class RestaurantListAPIView(generics.ListCreateAPIView):
+class RestaurantListCreateView(generics.ListCreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class RestaurantDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
 
 
-class RestaurantDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantSerializer
+class MenuItemListCreateView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-class FoodItemListAPIView(generics.ListCreateAPIView):
-    queryset = FoodItem.objects.all()
-    serializer_class = FoodItemSerializer
-
-
-class FoodItemDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = FoodItem.objects.all()
-    serializer_class = FoodItemSerializer
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
-
-
-class RestaurantListAPIView(generics.ListCreateAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-
-        user = self.request.user
-
-        if user.user_type != 'restaurant':
-            raise PermissionDenied("Only restaurant owners can create restaurants")
-
-        serializer.save(owner=user)
-
-from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
-from .models import Restaurant, FoodItem
-from .serializers import RestaurantSerializer, FoodItemSerializer
-
-
-class RestaurantListAPIView(generics.ListCreateAPIView):
-    queryset = Restaurant.objects.all().select_related('owner')
-    serializer_class = RestaurantSerializer
-
-    filter_backends = [
-        filters.SearchFilter,
-        DjangoFilterBackend
-    ]
-
-    search_fields = ['name', 'address']
-from rest_framework.permissions import IsAuthenticated
-from .models import Review
-from .serializers import ReviewSerializer
-class CreateReviewAPIView(generics.CreateAPIView):
-    serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
